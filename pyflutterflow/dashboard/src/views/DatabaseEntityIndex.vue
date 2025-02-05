165fd3898f7e75b09a3c435224347c6ba4e2a24a
John Kealy <johnckealy.dev@gmail.com>
2025-02-05 16:08:15 +0000

    update config displays in dashboard

--- pyflutterflow/dashboard/src/views/DatabaseEntityIndex.vue
+++ pyflutterflow/dashboard/src/views/DatabaseEntityIndex.vue
@@ -19,13 +19,28 @@
                         class="block w-full transition-all duration-300 ease-in-out">
                         <div
                             class="bg-surface-0 dark:bg-surface-900 border border-surface-200 dark:border-surface-700 rounded-lg shadow hover:shadow-md p-4 transition-all duration-300 ease-in-out">
-                            <span v-if="schema.fields[0].type === 'Date'"
-                                class="text-lg font-medium text-surface-700 dark:text-surface-200">
-                                {{ formatDate(databaseEntity[schema.fields[0].fieldName]) }}
-                            </span>
-                            <span v-else class="text-lg font-medium text-surface-700 dark:text-surface-200">
-                                {{ databaseEntity[schema.fields[0].fieldName] }}
-                            </span>
+                            <div class="text-lg font-medium text-surface-700 dark:text-surface-200">
+                                <span v-if="schema.label && schema.label.type === 'Date'">
+                                    {{ formatDate(databaseEntity[schema.label.fieldName]) }}
+                                </span>
+                                <span v-else-if="schema.label">
+                                    {{ databaseEntity[schema.label.fieldName] }}
+                                </span>
+                                <span v-else>
+                                    {{ databaseEntity[schema.fields[0].fieldName] }}
+                                </span>
+                            </div>
+                            <div class="text-surface-500">
+                                <span v-if="schema.sublabel && schema.sublabel.type === 'Date'">
+                                    {{ formatDate(databaseEntity[schema.sublabel.fieldName]) }}
+                                </span>
+                                <span v-else-if="schema.sublabel">
+                                    {{ databaseEntity[schema.sublabel.fieldName] }}
+                                </span>
+                                <span v-else>
+                                    {{ databaseEntity[schema.fields[1].fieldName] }}
+                                </span>
+                            </div>
                         </div>
                     </router-link>
                     <div v-if="schema.fields[0].type === 'Date' && formatDate(databaseEntity[schema.fields[0].fieldName]).includes('Friday')"
@@ -61,6 +76,8 @@ const schema = ref({})
 onMounted(async () => {
     schema.value = authStore.dashboardConfig.models.find(obj => obj.collection_name === route.params.entity);
 })
+
+
 
 const formatDate = (dateStr) => {
     if (!dateStr) return '';
